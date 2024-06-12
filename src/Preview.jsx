6afdd5d39f81de247c2c82adb406f72428f8f47a
Rohit singh <rohitsingh17031995@gmail.com>
2024-06-12 10:31:28 +0530

    Add files via upload

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const Preview = ({title,data}) => {
+const Preview = ({ title, data }) => {
   return (
     <div style={{ borderLeft: "1px solid black", flex: 1 }}>
-    <div>{title}</div>
-  </div>
-  )
-}
+      <div>{title}</div>
+      <div>
+        {data?.map((procedure, index) => {
+          return (
+            <div>
+              <div key={index} className="ps-4">{index + 1}. {procedure?.value}</div>
+              <div className="ps-5">
+                {procedure?.subHeading?.map((subHeading, subIndex) => {
+                  return (
+                    <div>
+                      <div>{subHeading?.value}</div>
+                      <div className="ps-4">
+                        {subHeading?.firstLevel?.map((firstLevel, firstIndex) => {
+                          return (
+                            <div>
+                              <div>{firstLevel?.value}</div>
+                              <div className="ps-4">
+                              {firstLevel?.secondLevel?.map((secondLevel, secondIndex) => {
+                                return (
+                                  <div>
+                                    <div>{secondLevel?.value}</div>
+                                  </div>
+                                );
+                              })}
+                              </div>
+                              
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Preview
+export default Preview;
